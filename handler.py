84efc7c75801c10f8bf82b6d68651e33e7ed56e7
Jorge Raze <soy.el.raze@gmail.com>
2020-09-22 17:50:18 -0500

    Update handler

--- handler.py
+++ handler.py
@@ -1,8 +1,11 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-DRIVER_PATH='/opt/chrome/chromedriver'
+DRIVER_PATH= f"{os.environ['CHROMEDRIVER_PATH']}chromedriver"
 
+print(DRIVER_PATH)
 options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
